68ba0c07ca319ef8040342cd556c3a7977aa28ba
Jose Manuel Valdivia Romero <jose.valdivia@native-instruments.de>
2025-03-22 12:45:38 +0100

    add basic random movement

--- Matlab/NextAction.m
+++ Matlab/NextAction.m
@@ -1,25 +1,42 @@
 function  NextAction(app)
-
 %perform an action based on current configuration
-switch app.LastCommand
-		case 'Rotate'
-			%StopMovement(app);
-        	forwBackVel = 0; %-1, +1
-            leftRightVel = 0; 
-            rotVel = app.inputRotationDirection;
-		case 'Translate'
-			%StopMovement(app);
-            forwBackVel = 2*app.inputTranslationDirection;
-            leftRightVel = 0;
-            rotVel = 0;
-		case 'Stop'
-			forwBackVel = 0;
-            leftRightVel = 0;
-            rotVel = 0;
-    otherwise
-        ClearInput(app);
-        return;
-end	
+%% 
+if all(app.RandomMovementButton.BackgroundColor == [0.96 0.96 0.96])
+    switch app.LastCommand
+		    case 'Rotate'
+			    %StopMovement(app);
+        	    forwBackVel = 0; %-1, +1
+                leftRightVel = 0; 
+                rotVel = app.inputRotationDirection;
+		    case 'Translate'
+			    %StopMovement(app);
+                forwBackVel = 2*app.inputTranslationDirection;
+                leftRightVel = 0;
+                rotVel = 0;
+		    case 'Stop'
+			    forwBackVel = 0;
+                leftRightVel = 0;
+                rotVel = 0;
+        otherwise
+            ClearInput(app);
+            return;
+    end
+else
+   switch app.randomDirection
+       case "forward"
+           forwBackVel = 1;
+           rotVel = 0;
+           leftRightVel = 0;
+       case "right"
+           forwBackVel = 0;
+           rotVel = 1;
+           leftRightVel = 0;
+       case "left"
+           forwBackVel = 0;
+           rotVel = -1;
+           leftRightVel = 0;
+   end
+end
 
 app.sim.simxSetJointTargetVelocity(app.clientID, app.h(1),-forwBackVel-leftRightVel-rotVel, app.sim.simx_opmode_streaming);
 app.sim.simxSetJointTargetVelocity(app.clientID, app.h(2),-forwBackVel+leftRightVel-rotVel, app.sim.simx_opmode_streaming);
